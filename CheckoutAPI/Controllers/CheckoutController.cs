ed5bf51cad1d0e26c1ed09b25e14f7c32e2f2db3
ChrisSorrell <chris.s.sorrell@gmail.com>
2023-07-23 18:45:18 -0400

    added Checkout method
    Signed-off-by: ChrisSorrell <chris.s.sorrell@gmail.com>

--- CheckoutAPI/Controllers/CheckoutController.cs
+++ CheckoutAPI/Controllers/CheckoutController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace CheckoutAPI.Controllers
 {
     [ApiController]
@@ -13,10 +14,10 @@ namespace CheckoutAPI.Controllers
     {
         private static readonly List<Watch> watches = new List<Watch>()
         {
-            new Watch("0001","Rolex",100),
-            new Watch("0002","Michael Kors",80),
-            new Watch("0003","Swatch",50),
-            new Watch("0004","Casio",30)
+            new Watch("001","Rolex",100, new Tuple<int, decimal>(3,200)),
+            new Watch("002","Michael Kors",80,new Tuple<int, decimal>(2,120)),
+            new Watch("003","Swatch",50),
+            new Watch("004","Casio",30)
         };
 
         private readonly ILogger<CheckoutController> _logger;
@@ -26,10 +27,61 @@ namespace CheckoutAPI.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
+        /// <summary>
+        /// Review the list of available watches in the catalogue
+        /// </summary>
+        [HttpGet("GetWatchCatalogue")]
         public IEnumerable<Watch> GetWatchCatalogue ()
         {
             return watches.ToArray();
         }
+
+        /// <summary>
+        /// Calculate total cost for checkout
+        /// </summary>
+        [HttpPost]
+        public decimal Checkout(List<string>itemIds)
+        {
+            var itemGroups = itemIds.GroupBy(t => t).Select(group => new
+            {
+                itemName = group.Key,
+                itemCount = group.Count()
+            });
+
+            decimal total = 0;
+            foreach (var itemGroup in itemGroups)
+                total += GetItemGroupPrice(itemGroup.itemName, itemGroup.itemCount);
+            
+
+
+            return total;
+        }
+
+        private decimal GetItemGroupPrice(string id, int count){
+            var item = watches.Where(t => t.Id == id).FirstOrDefault();
+            if (item != null)
+            {
+                if (item.HasDiscount())
+                {
+                    var unitsRequiredForDiscount = item.Discount.Item1;
+                    var discountedPrice = item.Discount.Item2;
+                    //get total for all discounted items
+                    var total = (count / unitsRequiredForDiscount) * discountedPrice;
+
+                    // add full price on remainder of items that don't quality for the discount
+                    total += (count % unitsRequiredForDiscount) * item.UnitPrice;
+
+                    return total;
+
+                }
+                else
+                {
+                    return item.UnitPrice * count;
+                }
+            }
+            return 0;
+
+        }
+
     }
 }
--- CheckoutAPI/Models/Watch.cs
+++ CheckoutAPI/Models/Watch.cs
@@ -10,15 +10,21 @@ namespace CheckoutAPI
 
         public decimal UnitPrice { get; set; }
 
-        public bool HasDiscount { get; set; }
 
-        public Watch(string id, string name, decimal price)
+        // item1 = items needed to apply discount. item2 = applied discount price.
+        public Tuple<int,decimal> Discount { get; set; }
+        
+
+        public Watch(string id, string name, decimal price, Tuple<int,decimal> discount = null)
         {
             Id = id;
             Name = name;
-            UnitPrice = price;
-
+            UnitPrice = price;           
+            Discount = discount;
         }
+
+        public bool HasDiscount() { return Discount != null;  }
+
     }
 }
     
